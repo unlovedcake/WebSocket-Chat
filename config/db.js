358310069022ee7b003a830365d7037101211fe1
Mon143 <mon.firstbostonsoftware@gmail.com>
2024-12-06 10:47:56 +0800

    add env msql port

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const db = mysql.createConnection({
   user:  process.env.MYSQL_USER,
   password:  process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 });
 
 db.connect(err => {
--- database.js
+++ database.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
   user:  process.env.MYSQL_USER,
   password:  process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 });
 
 
